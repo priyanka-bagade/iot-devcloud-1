925c58cfe2cc47d06c1987ea6fd53bbb3cfbf9c4
trexdog <38870270+trexdog@users.noreply.github.com>
2020-03-13 20:22:38 -0600

    make demo job file match sample's

--- python/demos/intruder-detector-python/intruder_detector.sh
+++ python/demos/intruder-detector-python/intruder_detector.sh
@@ -1,22 +1,25 @@
 
-#The default path for the job is your home directory, so we change directory to where the files are.
-cd $PBS_O_WORKDIR
-
-#intruder_detector script writes output to a file inside a directory. We make sure that this directory exists.
-#The output directory is the first argument of the bash script
+# Store input argumenst: <output_directory> <device> <fp_precision>
 OUTPUT_FILE=$1
 DEVICE=$2
 FP_MODEL=$3
-mkdir -p $1
+
+# The default path for the job is the user's home directory,
+#  change directory to where the files are.
+cd $PBS_O_WORKDIR
+
+# Make sure that the output directory exists.
+mkdir -p $OUTPUT_FILE
 
 if [ "$DEVICE" = "HETERO:FPGA,CPU" ]; then
-    # Environment variables and compilation for edge compute nodes with FPGAs - Updated for OpenVINO 2020.1
+    # Environment variables and compilation for edge compute nodes with FPGAs
     source /opt/altera/aocl-pro-rte/aclrte-linux64/init_opencl.sh
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/intel/openvino/bitstreams/a10_vision_design_sg1_bitstreams/BSP/a10_1150_sg1/linux64/lib
     aocl program acl0 /opt/intel/openvino/bitstreams/a10_vision_design_sg1_bitstreams/2019R4_PL1_FP11_YoloV3_ELU.aocx
     export CL_CONTEXT_COMPILER_MODE_INTELFPGA=3
 fi
 
+# Choose infernece model IR files based on required precision
 SAMPLEPATH=${PBS_O_WORKDIR}
 if [ "$FP_MODEL" = "FP32" ]; then
   MODELPATH=${SAMPLEPATH}/models/intel/person-vehicle-bike-detection-crossroad-0078/FP32/person-vehicle-bike-detection-crossroad-0078.xml
